7f8e2a4e4126729b7c15a44803a70aed204704ec
Quentin Lassalle <qlassalle@gmail.com>
2017-04-27 10:54:25 +0200

    Initialisation Spring Boot

---
+++ src/main/java/com/paris10/ent/EntApplication.java
@@ -0,0 +1,12 @@
+package com.paris10.ent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntApplication.class, args);
+	}
+}
