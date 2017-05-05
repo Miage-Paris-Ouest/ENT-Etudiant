9f54bc74c46e146fcf9216dc19cd713451e6c657
flthiebl <florian.thieblemont@gmail.com>
2017-05-05 09:38:02 +0200

    Ajout des méthodes pour l'upload

--- src/main/java/com/paris10/ent/controllers/EtudiantController.java
+++ src/main/java/com/paris10/ent/controllers/EtudiantController.java
@@ -3,6 +3,7 @@ import com.paris10.ent.entities.*;
 import com.paris10.ent.repositories.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = "/etudiant")
 public class EtudiantController {
 
